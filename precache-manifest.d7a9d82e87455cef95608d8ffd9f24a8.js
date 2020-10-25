36f7a30b21674ce1387cc711184853a7a57b3af2
Nick_Khoroshkov <nickolaykhoroshkov@gmail.com>
2020-10-25 20:19:05 +0200

    Updates

---
+++ precache-manifest.d7a9d82e87455cef95608d8ffd9f24a8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1840e2d3f40d6cb1111314d76225f70",
+    "url": "/hangman/index.html"
+  },
+  {
+    "revision": "3a53575a368631db1bf6",
+    "url": "/hangman/static/css/main.667aa0c9.chunk.css"
+  },
+  {
+    "revision": "973e31a4495480d1933b",
+    "url": "/hangman/static/js/2.5da62bfa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/hangman/static/js/2.5da62bfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a53575a368631db1bf6",
+    "url": "/hangman/static/js/main.79441d44.chunk.js"
+  },
+  {
+    "revision": "7ef95293683d0ed87720",
+    "url": "/hangman/static/js/runtime-main.6b115b9b.js"
+  },
+  {
+    "revision": "214f483785fe339c02473aa4d1a84b81",
+    "url": "/hangman/static/media/pumpkin.214f4837.svg"
+  },
+  {
+    "revision": "8519d15cff5143fac4fa3a6c37051c8f",
+    "url": "/hangman/static/media/witch.8519d15c.svg"
+  }
+]);
